998dc825eca058d8fdd4445ceefe38a45177b43c
aolvera8a <aolvera8a@griddynamics.com>
2022-11-08 15:46:24 -0600

    Modified api.yml to comply with requirements for endpoints, implemented generated interfaces in controllers

--- src/main/java/com/gd/clinic/controller/DoctorController.java
+++ src/main/java/com/gd/clinic/controller/DoctorController.java
@@ -13,39 +13,67 @@ import java.util.List;
 public class DoctorController implements DoctorsApi {
 
   @Override
-  public ResponseEntity<Void> addPatient(final PatientDto patientDto) {
+  public ResponseEntity<Void> addPatient(PatientDto patientDto) {
     return null;
   }
 
   @Override
-  public ResponseEntity<Void> createPrescription(final PrescriptionDto prescriptionDto) {
+  public ResponseEntity<Void> createPrescription(PrescriptionDto prescriptionDto) {
     return null;
   }
 
   @Override
-  public ResponseEntity<Void> deletePrescriptionById(final Long prescriptionId) {
+  public ResponseEntity<Void> deletePrescriptionById(Long prescriptionId) {
     return null;
   }
 
   @Override
-  public ResponseEntity<Void> dischargePatientByInsurance(final Long insuranceNumber) {
+  public ResponseEntity<Void> dischargePatientByInsurance(Long insuranceNumber) {
     return null;
   }
 
   @Override
-  public ResponseEntity<List<EventDto>> getAllEvents() {
+  public ResponseEntity<PatientDto> editPatient(Long patientId, String patientParameter, String parameterValue) {
     return null;
   }
 
   @Override
-  public ResponseEntity<Void> modifyEvent(final Long eventId, final EventDto eventDto) {
+  public ResponseEntity<List<EventDto>> getAllEventsFor(Long patientId) {
     return null;
   }
 
   @Override
-  public ResponseEntity<PrescriptionDto> modifyPrescription(final Long prescriptionId,
-      final String prescriptionParameter, final String parameterValue) {
+  public ResponseEntity<List<PatientDto>> getAllPatients() {
     return null;
   }
 
+  @Override
+  public ResponseEntity<PatientDto> getPatient(Long patientId) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<PrescriptionDto> getPrescription(Long prescriptionId) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<List<PrescriptionDto>> getPrescriptionsOf(Long patientId) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<List<EventDto>> modEvent(Long eventId, String eventParameter, String parameterValue) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Void> modifyEvent(Long eventId, EventDto eventDto) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<PrescriptionDto> modifyPrescription(Long prescriptionId, String prescriptionParameter, String parameterValue) {
+    return null;
+  }
 }
--- src/main/java/com/gd/clinic/controller/NurseController.java
+++ src/main/java/com/gd/clinic/controller/NurseController.java
@@ -1,21 +1,14 @@
 package com.gd.clinic.controller;
 
 import com.gd.clinic.api.NursesApi;
-import com.gd.clinic.model.EventDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class NurseController implements NursesApi {
-    @Override
-    public ResponseEntity<Void> cancelEvent(Long eventId, String cancelReason) {
-        return null;
-    }
 
     @Override
-    public ResponseEntity<List<EventDto>> modEvent(Long eventId, String eventParameter, String parameterValue) {
+    public ResponseEntity<Void> cancelEvent(Long eventId, String cancelReason) {
         return null;
     }
 }
