2eaf44c0d8e2cf14e349def058279552028095f0
Arturo Olvera Ochoa <aolvera@griddynamics.com>
2022-11-15 01:02:19 -0600

    Added requested changes to API, implemented changes in controllers

--- src/main/java/com/gd/clinic/controller/AdminController.java
+++ src/main/java/com/gd/clinic/controller/AdminController.java
@@ -3,12 +3,9 @@ package com.gd.clinic.controller;
 import com.gd.clinic.api.AdminApi;
 import com.gd.clinic.model.CredentialsDto;
 import com.gd.clinic.model.NewUserDto;
-import com.gd.clinic.model.TreatmentDto;
 import com.gd.clinic.model.UserResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class AdminController implements AdminApi {
