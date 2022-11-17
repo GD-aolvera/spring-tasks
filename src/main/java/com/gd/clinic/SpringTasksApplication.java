d7e2540abdfc64a8bdc9f338b5a93e358554c1c4
Arturo Olvera Ochoa <aolvera@griddynamics.com>
2022-11-17 13:44:57 -0600

    small clean-up in SpringTasksApplication

--- src/main/java/com/gd/clinic/SpringTasksApplication.java
+++ src/main/java/com/gd/clinic/SpringTasksApplication.java
@@ -6,12 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class
 SpringTasksApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTasksApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner run(UserRepo repo){
-//		return args -> {repo.save(new User("Arturo", "Olvera", "aolvera", "$2a$12$yccM2.hINPSDFmHkRatOKu3bY2ayZD3c33bs2jxkOm2r6qHQ7waS2"));};
-//	}
 }
