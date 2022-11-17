cd40fe7e804f3792a8574346b9f4b340197b8fda
GD-aolvera <112970830+GD-aolvera@users.noreply.github.com>
2022-11-17 17:02:51 -0600

    Open API (#10)
    * Added liquidbase to gradle
    
    * Added OpenAPI plugin for gradle dependency and created OAS yaml file
    
    * Added liquidbase and postgresql dependencies. As well as files needed for data migration
    
    * Converted changelog from sql to yml file
    
    * Converted 'application.properties' to yaml file
    
    * Updated gradle dependencies
    
    * Added OpenApi Generate command and modified api.yml
    
    * Fixed OpenApi plugin dependecy, now generating code
    
    * Added first version of OAS yaml file for the Hospital Management API
    
    * modified api.yml
    
    * Added last couple of features for the API
    
    * added openapi generator to the main build.gradle file, added opportunity to reach generator classes from main code
    
    * Made changes on changelog file
    
    * Removed creation of tables from .sql file
    
    * Temporary Commit
    
    * Corrected liquidbase dependency for upcomming merge when SQL is needed
    
    * Modified api.yml to comply with requirements for endpoints, implemented generated interfaces in controllers
    
    * Added most of the design corrections discussed @ meeting
    
    * Corrected compilation error, updated api.yml to newer version with corrections in DTO schemas
    
    * Added requested changes to API, implemented changes in controllers
    
    * For gradle clean build to work, I removed openapi plug in that pointed to the openapi.gradle file that is not included in master branch
    
    * Create README.md
    
    * Added docker files. Build .jar files and run docker once open-api and… (#7)
    
    * Added docker files. Build .jar files and run docker once open-api and security branches ar merged to master
    
    * [Alexey Korovko] changed Dockerfile and docker-compose.yml files
    
    * [Alexey Korovko] Dockerfile bug fix
    
    * Removed jpa dependency to allow program to run without any error due to no dataSource
    
    Co-authored-by: Sergio Ruiz <sruiz@Sergios-MBP.griddynamics.com>
    Co-authored-by: Alexey Korovko <akorovko@wsgc.com>
    
    Co-authored-by: Sergio Ruiz <sruiz@Sergios-MBP.griddynamics.com>
    Co-authored-by: aolvera8a <aolvera8a@griddynamics.com>
    Co-authored-by: Alexey Korovko <akorovko@wsgc.com>
    Co-authored-by: bluedoritos <112970773+bluedoritos@users.noreply.github.com>

--- src/main/java/com/gd/clinic/SpringTasksApplication.java
+++ src/main/java/com/gd/clinic/SpringTasksApplication.java
@@ -1,11 +1,11 @@
-package com.gd.spring;
+package com.gd.clinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTasksApplication {
-
+public class
+SpringTasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTasksApplication.class, args);
 	}
