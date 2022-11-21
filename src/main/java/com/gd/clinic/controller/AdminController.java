ab41443dff418be3307a39187f5d42449167a385
sergio <sruiz@griddynamics.com>
2022-11-21 16:21:31 -0600

    progress

--- src/main/java/com/gd/clinic/controller/AdminController.java
+++ src/main/java/com/gd/clinic/controller/AdminController.java
@@ -1,15 +1,20 @@
 package com.gd.clinic.controller;
 
 import com.gd.clinic.api.AdminApi;
+import com.gd.clinic.exception.AdminNotFoundException;
 import com.gd.clinic.model.CredentialsDto;
 import com.gd.clinic.model.NewUserDto;
 import com.gd.clinic.model.UserResponseDto;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.*;
 
+@Log4j2
 @RestController
 public class AdminController implements AdminApi {
-
+    //adminService
     @Override
     public ResponseEntity<UserResponseDto> createUser(NewUserDto newUserDto) {
         return null;
@@ -18,5 +23,12 @@ public class AdminController implements AdminApi {
     @Override
     public ResponseEntity<Void> login(CredentialsDto credentialsDto) {
         return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> getAdmin(CredentialsDto credentialsDto) throws AdminNotFoundException {
+        //Log.debug("entered getadmin");
+        AdminApi a= adminService.getbyID();
+        return ResponseEntity.ok(a);
     }
 }
---
+++ src/main/java/com/gd/clinic/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.gd.clinic.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(){
+
+    }
+}
