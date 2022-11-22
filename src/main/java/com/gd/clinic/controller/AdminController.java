0ad04f0881f9837066556769f59b1de27f4f86f7
sergio <sruiz@griddynamics.com>
2022-11-22 11:31:18 -0600

    Added GlobalExceptionHandler

--- src/main/java/com/gd/clinic/controller/AdminController.java
+++ src/main/java/com/gd/clinic/controller/AdminController.java
@@ -1,17 +1,14 @@
 package com.gd.clinic.controller;
 
 import com.gd.clinic.api.AdminApi;
-import com.gd.clinic.exception.AdminNotFoundException;
 import com.gd.clinic.model.CredentialsDto;
 import com.gd.clinic.model.NewUserDto;
 import com.gd.clinic.model.UserResponseDto;
-import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j2;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import javax.validation.*;
 
-@Log4j2
+
 @RestController
 public class AdminController implements AdminApi {
     //adminService
@@ -25,10 +22,5 @@ public class AdminController implements AdminApi {
         return null;
     }
 
-    @Override
-    public ResponseEntity<Void> getAdmin(CredentialsDto credentialsDto) throws AdminNotFoundException {
-        //Log.debug("entered getadmin");
-        AdminApi a= adminService.getbyID();
-        return ResponseEntity.ok(a);
-    }
+
 }
--- src/main/java/com/gd/clinic/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gd/clinic/exception/GlobalExceptionHandler.java
@@ -1,12 +1,42 @@
 package com.gd.clinic.exception;
 
+import com.gd.clinic.model.ErrorResponseDto;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+import org.springframework.web.server.ResponseStatusException;
+
+
+
+import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(){
+@Slf4j
+public class GlobalExceptionHandler  {
 
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponseDto> generateException(ResponseStatusException ex)
+    {
+        ErrorResponseDto dto = new ErrorResponseDto();
+        dto.setStatus( String.valueOf( ex.getStatus().value()));
+        dto.setMessage(ex.getMessage());
+        log.error("Exception Occurred : ", ex);
+
+        return new ResponseEntity<ErrorResponseDto>(dto,ex.getStatus());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponseDto> generateException(RuntimeException ex)
+    {
+        ErrorResponseDto dto = new ErrorResponseDto();
+        dto.setStatus("500");
+        dto.setMessage(ex.getMessage());
+        log.error("Exception Occurred : ",ex);
+
+        return new ResponseEntity<>(dto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
